64c6cd53991d56f9fb341e9375c98110c208cba1
Daniil Iskhakov <shkiper325@gmail.com>
2024-03-23 13:39:04 +0300

    Добавил недостающий заголовок stl

--- experiments/utils/utils.h
+++ experiments/utils/utils.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 
+#include <cstdint>
+
 std::string rootPath();
 std::string readFile(const std::string &filename);
 std::vector<uint8_t> readFileBinary(const std::string &filename);
