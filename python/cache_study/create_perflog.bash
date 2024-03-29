05d9672993b25ff46a81071fb0034fcfa2459a4e
Daniil Iskhakov <shkiper325@gmail.com>
2024-03-29 10:20:26 +0300

    Добавлен скрипт для прогона perf

---
+++ python/cache_study/create_perflog.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+iters=20
+
+echo "n: stock"
+rm -rf blob/stock.perflog
+
+executable="./blob/stock_simulator"
+for iter in $(seq $iters); do
+    echo "$iter/$iters"
+
+    output=$(perf stat -B -e cache-misses,cache-references $executable 1 2>&1)
+    echo "\"$output\"" >> blob/stock.perflog
+done
+
+for n in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096; do
+    echo "n: $n"
+    rm -rf blob/$n.perflog
+
+    executable="./blob/greedy${n}_simulator"
+    for iter in $(seq $iters); do
+        echo "$iter/$iters"
+
+        output=$(perf stat -B -e cache-misses,cache-references $executable 1 2>&1)
+        echo "\"$output\"" >> blob/$n.perflog
+    done
+done
