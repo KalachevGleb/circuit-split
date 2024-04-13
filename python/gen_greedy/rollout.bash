acacc609467a47e1f7a8f18a22bc39e7546b084b
Daniil Iskhakov <shkiper325@gmail.com>
2024-04-13 20:00:31 +0300

    Доработан скрипт роллаута

--- python/gen_greedy/rollout.bash
+++ python/gen_greedy/rollout.bash
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 mkdir -p rollouts
-rm -rf rollouts
-mkdir rollouts
 
-for threads in 1 2 4 8; do
+losses=$(echo 1; seq 10 10 100; seq 200 100 1000; seq 2000 500 11000)
+
+for threads in 2; do
     mkdir -p "rollouts/$threads"
-    for LOSS1 in 0 1 10 20 30 40 50 60 70 80 90 100: do
-        for LOSS2 in 0 1 10 20 30 40 50 60 70 80 90 100; do
-            python gen.py $threads $LOSS1 $LOSS2 dep_graph.json cut.txt
-            simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10 >> "rollouts/$threads/${LOSS1}_${LOSS2}.txt"
+
+    for LOSS1 in $losses; do
+        for LOSS2 in $losses; do
+            if test -f "rollouts/${threads}/${LOSS1}_${LOSS2}.txt"; then
+                continue
+            fi
+            
+            output=$(python gen.py 2 $LOSS1 $LOSS2  bitonic10.json cut.json)
+            single_threaded_cost=$(echo "$output" | grep -oE 'Стоимость однопоточного вычисления: ([0-9]+\.*)' | awk '{print $NF}')
+            cost=$(echo "$output" | grep -oE '[0-9]+\.[0-9]+' | awk 'NR<=2 {printf "%s%s", $1, (NR<2 ? "," : "\n")}')
+            echo "$threads, $LOSS1, $LOSS2, $single_threaded_cost, $cost " >> rollout.csv
+            
+            simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10 > "rollouts/${threads}/${LOSS1}_${LOSS2}.txt"
         done
     done
 done
