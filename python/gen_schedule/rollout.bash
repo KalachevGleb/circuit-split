982c510de3367b7b513991c247b46322adbf6339
Daniil Iskhakov <shkiper325@gmail.com>
2024-04-17 13:21:16 +0300

    Для эксперимента модифицирован rollout.bash

--- python/gen_schedule/rollout.bash
+++ python/gen_schedule/rollout.bash
@@ -2,23 +2,24 @@
 
 mkdir -p rollouts
 
-losses=$(echo 1; seq 10 10 100; seq 200 100 1000; seq 2000 500 11000)
+for scheme in rollout_graphs/*; do
+    for threads in 2 3 4 5 6 7 8; do
+        name=$(basename ${scheme} .json)
 
-for threads in 2; do
-    mkdir -p "rollouts/$threads"
+        if test -f "rollouts/${name}_${threads}.txt"; then
+            continue
+        fi
+        
+        output=$(python gen.py ${threads} 1 0 ${scheme} cut.json)
 
-    for LOSS1 in $losses; do
-        for LOSS2 in $losses; do
-            if test -f "rollouts/${threads}/${LOSS1}_${LOSS2}.txt"; then
-                continue
-            fi
-            
-            output=$(python gen.py 2 $LOSS1 $LOSS2  bitonic10.json cut.json)
-            single_threaded_cost=$(echo "$output" | grep -oE 'Стоимость однопоточного вычисления: ([0-9]+\.*)' | awk '{print $NF}')
-            cost=$(echo "$output" | grep -oE '[0-9]+\.[0-9]+' | awk 'NR<=2 {printf "%s%s", $1, (NR<2 ? "," : "\n")}')
-            echo "$threads, $LOSS1, $LOSS2, $single_threaded_cost, $cost " >> rollout.csv
-            
-            simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10 > "rollouts/${threads}/${LOSS1}_${LOSS2}.txt"
-        done
+        single_thread_cost=$(echo "$output" | grep -oE 'Стоимость однопоточного вычисления: ([0-9]+\.*)' | awk '{print $NF}')
+        barriers=$(echo "$output" | grep -oE 'Барьеров: ([0-9]+\.*)' | awk '{print $NF}')
+        util=$(echo "$output" | grep -oE 'Утилизация потоков: ([0-9]+\.[0-9]+*)' | awk '{print $NF}')
+        #cost=$(echo "$output" | grep -oE '[0-9]+\.[0-9]+' | awk 'NR<=2 {printf "%s%s", $1, (NR<2 ? "," : "\n")}')
+
+        echo "$name, $threads, $single_thread_cost, $barriers, $util"  >> rollout.csv
+        
+        rm -rf blob
+        simulation cut.json blob/work/ --compiler /usr/bin/clang++ --run --time 10 > "rollouts/${name}_${threads}.txt"
     done
 done
