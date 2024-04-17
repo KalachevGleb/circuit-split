7c3f9b1cc3f7675552266f35c39f028bce48da55
Daniil Iskhakov <shkiper325@gmail.com>
2024-04-17 10:14:24 +0300

    Теперь test.bash приминаем на вход путь до файла с расписанием

--- python/gen_schedule/test.bash
+++ python/gen_schedule/test.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo 
-python gen.py 1 $2 $3 graphs/bitonic13.json cut.json
+python gen.py 1 $2 $3 $4 cut.json
 simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10
 echo
-python gen.py $1 $2 $3 graphs/bitonic13.json cut.json
+python gen.py $1 $2 $3 $4 cut.json
 simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10
 echo
