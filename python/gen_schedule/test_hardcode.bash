03227d972791ba4b8157c2bf3302060cf755cbc2
Daniil Iskhakov <shkiper325@gmail.com>
2024-04-19 17:08:30 +0300

    Подготовил эксперимент по пересмешиваю вершин в слоях послойного расписания и по распределению вершин слоя по потокам - dummy и backpack

--- python/gen_schedule/test_hardcode.bash
+++ python/gen_schedule/test_hardcode.bash
@@ -1,26 +1,46 @@
 #!/bin/bash
 
+ts=$(date +”%A, %b %d, %Y %H:%M:%S”	)
+bin_dir="bin/$ts"
+
 rm -rf blob
-mkdir -p bin
+mkdir -p "$bin_dir"
+cp ./test_hardcode.bash "$bin_dir"
 
 echo 
 python gen.py 1 -1 -1 graphs/600k.json cut.json
-simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10
-cp blob/work/generated_code/bin/simulator ./bin/600k_1_-1_-1
+simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 30 >> "$bin_dir/log.txt"
+cp blob/work/generated_code/bin/simulator ./$bin_dir/600k_1_-1_-1
 echo 
-python gen.py 4 1 0 graphs/600k.json cut.json
-simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10
-cp blob/work/generated_code/bin/simulator ./bin/600k_4_1_0
+python gen.py 2 1 0 graphs/600k.json cut.json --inside_layer_schedule dummy
+simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 30 >> "$bin_dir/log.txt"
+cp blob/work/generated_code/bin/simulator ./$bin_dir/600k_2_1_0_dummy
 echo 
-python gen.py 8 1 0 graphs/600k.json cut.json
-simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10
-cp blob/work/generated_code/bin/simulator ./bin/600k_8_1_0
+python gen.py 2 1 0 graphs/600k.json cut.json --inside_layer_schedule dummy --shuffle_layers
+simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 30 >> "$bin_dir/log.txt"
+cp blob/work/generated_code/bin/simulator ./$bin_dir/600k_2_1_0_dummy_shuffle
 echo 
-python gen.py 12 1 0 graphs/600k.json cut.json
-simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10
-cp blob/work/generated_code/bin/simulator ./bin/600k_12_1_0
+python gen.py 2 1 0 graphs/600k.json cut.json --inside_layer_schedule backpack
+simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 30 >> "$bin_dir/log.txt"
+cp blob/work/generated_code/bin/simulator ./$bin_dir/600k_2_1_0_backpack
 echo 
-python gen.py 16 1 0 graphs/600k.json cut.json
-simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10
-cp blob/work/generated_code/bin/simulator ./bin/600k_16_1_0
+python gen.py 2 1 0 graphs/600k.json cut.json --inside_layer_schedule backpack --shuffle_layers
+simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 30 >> "$bin_dir/log.txt"
+cp blob/work/generated_code/bin/simulator ./$bin_dir/600k_2_1_0_backpack_shuffle
+echo 
+python gen.py 4 1 0 graphs/600k.json cut.json --inside_layer_schedule dummy
+simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 30 >> "$bin_dir/log.txt"
+cp blob/work/generated_code/bin/simulator ./$bin_dir/600k_4_1_0_dummy
+echo
+python gen.py 4 1 0 graphs/600k.json cut.json --inside_layer_schedule dummy --shuffle_layers
+simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 30 >> "$bin_dir/log.txt"
+cp blob/work/generated_code/bin/simulator ./$bin_dir/600k_4_1_0_dummy_shuffle
+echo
+python gen.py 4 1 0 graphs/600k.json cut.json --inside_layer_schedule backpack
+simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 30 >> "$bin_dir/log.txt"
+cp blob/work/generated_code/bin/simulator ./$bin_dir/600k_4_1_0_backpack
+echo
+python gen.py 4 1 0 graphs/600k.json cut.json --inside_layer_schedule backpack --shuffle_layers
+simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 30 >> "$bin_dir/log.txt"
+cp blob/work/generated_code/bin/simulator ./$bin_dir/600k_4_1_0_dummy_backpack_shuffle
 echo
