45cff6ac5f2cd76aa5d30dcfb9570875fd613fa3
Daniil Iskhakov <shkiper325@gmail.com>
2024-04-20 16:44:43 +0300

    Добавлен скрипт для роллаута отдельных слоев больших графов

---
+++ python/gen_schedule/rollout_layers.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+mkdir -p rollouts
+rm -rf rollout.csv
+
+threads=2
+scheme="$1"
+
+for sd in 2 3 4 5; do
+    rm -rf cutted_graphs
+    python gen.py ${threads} 1 0 ${scheme} cut.json --kill_layers True --extracted_layer_start 2 --extracted_layer_stop 40 --survive_depth $sd
+    
+    for cutted_scheme in cutted_graphs/*; do
+    echo "python gen.py ${threads} 1 0 ${cutted_scheme} cut.json"
+        output=$(python gen.py ${threads} 1 0 ${cutted_scheme} cut.json)
+
+        single_thread_cost=$(echo "$output" | grep 'Стоимость однопоточного вычисления:' | grep -oE '\b([0-9]+)')
+
+        A=$(echo "$output" | grep 'Суммарно чтений внутри одного потока:' | grep -oE '\b[0-9]+(\.[0-9]+)?')
+        B=$(echo "$output" | grep 'Суммарно чтений между потоками:' | grep -oE '\b[0-9]+(\.[0-9]+)?')
+        C=$(echo "$output" | grep 'Суммарно записей:' | grep -oE '\b[0-9]+(\.[0-9]+)?')
+
+        util=$(echo "$output" | grep 'Утилизация потоков:' | grep -oE '\b[0-9]+(\.[0-9]+)?')
+
+        barriers=$(echo "$output" | grep 'Барьеров:' | grep -oE '\b([0-9]+)')
+
+        out="$threads, $single_thread_cost, $barriers"
+        for a in $A; do out="$out, $a"; done
+        for b in $B; do out="$out, $b"; done
+        for c in $C; do out="$out, $c"; done
+        out="$out, $util"
+        X=$(echo $out | tr -d '\n')
+        
+        rm -rf blob
+        output=$(simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 3)
+
+        ns_per_node=$(echo "$output" | grep ns_per_node | grep -oE '[0-9]+(\.[0-9]+)')
+        ns_per_read=$(echo "$output" | grep ns_per_read | grep -oE '[0-9]+(\.[0-9]+)')
+
+        Y="$ns_per_node, $ns_per_read"
+
+        echo "$X, $Y" >> rollout.csv
+    done
+done
