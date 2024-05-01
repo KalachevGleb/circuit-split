6d6850ad3fe9bbecd74e53b9c05e5f3752b802d4
Daniil Iskhakov <shkiper325@gmail.com>
2024-05-01 11:44:13 +0300

    Адаптирован скрипт для эксперимента с вычленением слоев из графа

--- python/gen_schedule/rollout_layers.bash
+++ python/gen_schedule/rollout_layers.bash
@@ -5,12 +5,12 @@ rm -rf rollout.csv
 threads=2
 scheme="$1"
 
-for sd in 2 3 4 5 6 7 8 9 10; do
+for sd in 2 3 4; do
     rm -rf cutted_graphs
-    python gen.py ${threads} 1 0 ${scheme} cut.json --kill_layers True --survive_depth $sd
+    python gen.py ${threads} 1 0 "${scheme}" cut.json --kill_layers True --survive_depth $sd
     
     for cutted_scheme in cutted_graphs/*; do
-        output=$(python gen.py ${threads} 1 0 ${cutted_scheme} cut.json)
+        output=$(python gen.py ${threads} 1 0 "${cutted_scheme}" cut.json)
 
         single_thread_cost=$(echo "$output" | grep 'Стоимость однопоточного вычисления:' | grep -oE '\b([0-9]+)')
 
@@ -22,12 +22,14 @@ for sd in 2 3 4 5 6 7 8 9 10; do
 
         barriers=$(echo "$output" | grep 'Барьеров:' | grep -oE '\b([0-9]+)')
 
-        out="$threads, $single_thread_cost, $barriers"
-        for a in $A; do out="$out, $a"; done
-        for b in $B; do out="$out, $b"; done
-        for c in $C; do out="$out, $c"; done
-        out="$out, $util"
-        X=$(echo $out | tr -d '\n')
+        name="$(basename "$cutted_scheme" .json)"
+
+        out="\"$name\",$threads,$single_thread_cost,$barriers"
+        for a in $A; do out="$out,$a"; done
+        for b in $B; do out="$out,$b"; done
+        for c in $C; do out="$out,$c"; done
+        out="$out,$util"
+        X=$(echo $out | tr -d '\n' | tr -d ' ')
         
         rm -rf blob
         output=$(simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 3)
@@ -35,8 +37,8 @@ for sd in 2 3 4 5 6 7 8 9 10; do
         ns_per_node=$(echo "$output" | grep ns_per_node | grep -oE '[0-9]+(\.[0-9]+)')
         ns_per_read=$(echo "$output" | grep ns_per_read | grep -oE '[0-9]+(\.[0-9]+)')
 
-        Y="$ns_per_node, $ns_per_read"
+        Y=$(echo "$ns_per_node,$ns_per_read" | tr -d '\n' | tr -d ' ')
 
-        echo "$X, $Y" >> rollout.csv
+        echo "$X,$Y" >> rollout.csv
     done
 done
