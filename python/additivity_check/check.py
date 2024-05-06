640386c9506d86d291bc4c2a12e8e2fb9942b183
Daniil Iskhakov <shkiper325@gmail.com>
2024-05-06 15:50:14 +0300

    Сильно переписал check.py в проверке гипотезы аддитивности. Получилось подобрать параметры для 13% отклонения на 50к схеме при "обучении" на слайсах 600k схемы

--- python/additivity_check/check.py
+++ python/additivity_check/check.py
@@ -1,62 +1,88 @@
 import numpy as np
-
+from tqdm import tqdm
 import pandas as pd
 
 from scipy.optimize import minimize
 
-raw_col_num = len(pd.read_csv('dataset.csv', nrows=0).columns)
+I = 7
 
-colnames = list(map(str, range(raw_col_num)))
-colnames[0] = 'filename'
-colnames[-2] = 'ns_per_node'
+def load_dataset(filename):
 
-df = pd.read_csv('dataset.csv', names=colnames, dtype={'filename' : str, 'ns_per_node' : float})
-df = df.loc[:, ['filename', 'ns_per_node']]
+    df = pd.read_csv(filename, dtype={'filename' : str, 
+                                      'ns_per_node' : str,
+                                      'vals1' : str,
+                                      'vals2' : str,
+                                      'vals3' : str
+                                      })
+    df = df.loc[:, ['filename', 'ns_per_node', 'vals1', 'vals2', 'vals3']]
 
-df['layers'] = df['filename'].apply(lambda x: eval(x.split('_')[-3]))
-df['sizes'] = df['filename'].apply(lambda x: eval(x.split('_')[-2]))
-del df['filename']
+    df['layers'] = df['filename'].apply(lambda x: eval(x.split('_')[-3]))
+    df['sizes'] = df['filename'].apply(lambda x: eval(x.split('_')[-2]))
+    del df['filename']
 
-data = df.to_numpy(dtype=object)
+    df['ns_per_node'] = df['ns_per_node'].apply(lambda x: np.mean(eval(x)))
 
-def gen_dataset(data):
+    df['vals1'] = df['vals1'].apply(lambda x: eval(x.split(':')[-1]))
+    df['vals2'] = df['vals2'].apply(lambda x: eval(x.split(':')[-1]))
+    df['vals3'] = df['vals3'].apply(lambda x: eval(x.split(':')[-1]))
+
     simple = dict()
     multi = dict()
-    for entry in data:
-        if len(entry[1]) == 2:
-            simple.update(
-                {entry[1][1] : entry}
-            )
-        elif len(entry[1]) == 3:
-            multi.update(
-                {entry[1][1] : entry}
-            )
+    def line2dict(line):
+        return {'sizes' : line['sizes'],
+                'ns_per_node' : line['ns_per_node'],
+                'vals1' : line['vals1'],
+                'vals2' : line['vals2'],
+                'vals3' : line['vals3']
+                }
+    
+    skipped_multi = 0
+    for index, line in df.iterrows():
+        if len(line['layers']) == 2:
+            simple.update({line['layers'][1] : line2dict(line)})
+        elif len(line['layers']) == I and line['ns_per_node'] < 5:
+            multi.update({line['layers'][1] : line2dict(line)})
+        elif len(line['layers']) == I:
+            skipped_multi += 1
+
+    print('Пропущено', str(skipped_multi) + '/' + str(len(multi) + skipped_multi))
 
     return simple, multi
 
-simple_train, multi_train = gen_dataset(data[:129])
-simple_test, multi_test = gen_dataset(data[129:])
+simple_train, multi_train = load_dataset('rollout_600k.csv')
+simple_test, multi_test = load_dataset('rollout_dep_graph.csv')
 
 print(len(multi_train))
 print(len(multi_test))
 
+buff = None
+
 def loss(x, simple, multi):
     x = np.array(x)
 
+    global buff
+    buff = []
     val = []
     for num in multi:
-        if simple[num][2][1] != multi[num][2][1] or simple[num+1][2][1] != multi[num][2][2]:
-            print('Error')
-            quit(1)
+        for i in range(I-1):
+            if simple[num+i]['sizes'][1] != multi[num]['sizes'][i+1]:
+                print('Error')
+                quit(1)
 
-        A = npn_connected = multi[num][0] #ns_per_node
+        target = multi[num]['ns_per_node']
+        
+        vals1 = multi[num]['vals1']
+        vals2 = multi[num]['vals2']
+        vals3 = multi[num]['vals3']
 
-        sz_0, sz_1, sz_2 = multi[num][2][0], multi[num][2][1], multi[num][2][2]
-        npn_1, npn_2 = simple[num][0], simple[num+1][0]
-        vec = np.array([sz_0, sz_1, sz_2, npn_1, npn_2, 1])
-        B = (sz_0 + sz_1 + sz_2) / ((sz_0 + sz_1) / npn_1 + (sz_1 + sz_2) / npn_2) + sz_0 * x[0] + sz_1 * x[1] + sz_2 * x[2] # + np.sum(x[:6] * vec)) + np.sum(x[6:] * vec)
-        val.append(np.abs(B / A - 1))
-
+        szs = np.array(multi[num]['sizes'])
+        npns = np.array([simple[num+i]['ns_per_node'] for i in range(I-1)])
+        
+        predicted = np.sum(np.array(npns) * x[:6]) + x[6] + (np.sum(szs)) / np.sum([(szs[i] + szs[i+1]) / npns[i] for i in range(I-1)])
+        val.append(np.abs(predicted - target))
+        buff.append(np.abs(predicted / target - 1))
+    
+    buff = np.mean(buff)
     return np.mean(val)
 
 def f1(x):
@@ -65,12 +91,12 @@ def f1(x):
 def f2(x):
     return loss(x, simple_test, multi_test)
 
-res = minimize(f1, x0=[0] * 12, method='L-BFGS-B')
-
-print(res.x)
-
-print(f1(res.x))
-print(f1([0] * 12))
-
-print(f2(res.x))
-print(f2([0] * 12))
+scores_train, scores_test = [], []
+for i in tqdm(range(20)):
+    res = minimize(f1, x0=np.random.randn(13) * 0.01, method='L-BFGS-B')
+    f1(res.x)
+    scores_train.append(buff)
+    f2(res.x)
+    scores_test.append(buff)
+print('Train:', scores_train[np.argmin(scores_test)])
+print('Test:', np.amin(scores_test))
