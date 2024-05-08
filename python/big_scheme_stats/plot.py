fa279fe5574095f2f217faea8332c4e4a920f3b6
Daniil Iskhakov <shkiper325@gmail.com>
2024-05-08 22:57:16 +0300

    Убрал зависимость от seaborn, теперь рисуются гистограммы через matplotlib
    Добавлена поддержка оценки среднего и дов интервала
    для данных

--- python/big_scheme_stats/plot.py
+++ python/big_scheme_stats/plot.py
@@ -2,13 +2,16 @@ import pickle
 
 import numpy as np
 
-import seaborn as sns
 from matplotlib import pyplot as plt
 from matplotlib import rcParams
 
 rcParams['figure.figsize'] = 20, 15
 
 GRID_SIZE = 10
+BINS = 50
+ALPHA = 0.05
+Q = 1.97196
+N = 200
 
 with open('log.pickle', 'rb') as fd:
     objects = pickle.load(fd)
@@ -24,11 +27,15 @@ for obj in objects:
     else:
         data[curr_command].append(obj)
 
+cmd_lengths = [len(cmd) for cmd in commands]
+print(cmd_lengths)
+
 npns = {cmd : [] for cmd in commands}
 for key, vals in data.items():
     npns[key] = [val['ns_per_node'] for val in vals]
 
-exp_nums = list(range(10))
+exp_nums = [i for i in range(10) if '4 1 0' in commands[i]]
+
 for exp_num in exp_nums:
     command = commands[exp_num]
     exp = np.array(npns[command])
@@ -43,9 +50,20 @@ for exp_num in exp_nums:
 
     plot_data = sum([[grid[i]] * bins[i] for i in range(GRID_SIZE)], [])
 
-    sns.set_style('whitegrid')
-    plot = sns.kdeplot(plot_data)
+    plt.hist(exp, bins=BINS, histtype='step')
 
-fig = plot.get_figure()
+    if exp.shape[0] != N:
+        print('Пропускается эксперимент', exp_num, ' т.к. нет соответствующей квантили')
+    else:
+        mu = np.mean(exp)
+        s = np.sqrt(np.sum((exp - mu) ** 2) / (N - 1))
+        low = mu - Q * s / np.sqrt(N)
+        high = mu + Q * s / np.sqrt(N)
+
+        print(('[' + command + ']').ljust(max(cmd_lengths) + 5) +  'среднее: ' + str(round(mu, 3)) + \
+              ', ди: [' + str(round(low, 3)) + ';' + str(round(high, 3)) + \
+                '], размер ди: ' + str(round(high - low, 3)))
+
+plt.xlim(0, None)
 plt.legend(title='proba_density', loc='upper right', labels=commands)
-fig.savefig('plot.png')
+plt.savefig('plot.png')
