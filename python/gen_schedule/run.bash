6cdd2fe45f3c5c02733722bf588142669dac8c42
Daniil Iskhakov <shkiper325@gmail.com>
2024-05-08 15:38:47 +0300

    Убрал ненужные скрипты

--- python/gen_schedule/run.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10
--- python/gen_schedule/test.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 
-python gen.py 1 $2 $3 $4 cut.json
-simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10
-echo
-python gen.py $1 $2 $3 $4 cut.json
-simulation cut.json blob/work/ --compiler /usr/bin/g++ --run --time 10
-echo
