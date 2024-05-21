9f641d09daac0833727a9298a50d39ce1f683e27
Daniil Iskhakov <shkiper325@gmail.com>
2024-05-21 14:46:24 +0300

    Поменял параметры скрипта роллаута

--- python/gen_schedule/temp.bash
+++ python/gen_schedule/temp.bash
@@ -5,10 +5,10 @@ if [ "$( cat /proc/sys/kernel/perf_event_paranoid )" != "-1" ]; then
     sudo sh -c 'echo -1 > /proc/sys/kernel/perf_event_paranoid'
 fi
 
-N=3
-thread_nums=(1 2)
-simple_widthes=(8 16)
-bitonic_widthes=(2 3)
+N=200
+thread_nums=(1 2 4)
+simple_widthes=(8 9 10 11 12 13 14 15 16)
+bitonic_widthes=(4 5 6 7 8 9 10 11 12 13 14)
 
 ts="$( date +"%d.%m.%Y %H:%M:%S" )"
 bin_dir="bin/$ts $(git rev-parse HEAD)"
