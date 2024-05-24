6b94341f37c6fa12851d3ec2016e33fbf39281b4
Daniil Iskhakov <shkiper325@gmail.com>
2024-05-24 18:46:25 +0300

    Поправил баг с использоавнием значения wdith как показателя степени

--- python/gen_schedule/temp.bash
+++ python/gen_schedule/temp.bash
@@ -7,7 +7,7 @@ fi
 
 N=200
 thread_nums=(1 2 4)
-simple_widthes=(7 8 9 10 11 12 13 14 15 16)
+simple_widthes=(128 256 512 1024 2048 4096 8192 16384 32768 65536 131072)
 bitonic_widthes=(4 5 6 7 8 9 10 11 12 13 14)
 
 ts="$( date +"%d.%m.%Y %H:%M:%S" )"
