d963e438228fa512bbec95abb7948aca32e84f3d
Daniil Iskhakov <shkiper325@gmail.com>
2024-09-07 16:23:56 +0300

    Набольшая модификация для сприпта сборки experiments

--- experiments/build.bash
+++ experiments/build.bash
@@ -5,3 +5,6 @@ mkdir build
 cd build
 cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
 cmake --build . --config Release -- -j$1
+cd ..
+
+cp ../python/cache_study/*.so ../python/gen_schedule/
